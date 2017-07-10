e71be4e6dae4b82143bc6d221c629e8495f62a48
Grant McDade <grant.mcdade@gmail.com>
2017-07-10 10:29:31 +0200

    Initial files

---
+++ build/config.js
@@ -0,0 +1,9 @@
+'use strict'
+
+module.exports = {
+  port: 4000,
+  title: 'stock-trader',
+  // when you use electron please set to relative path like ./
+  // otherwise only set to absolute path when you're using history mode
+  publicPath: '/'
+}
